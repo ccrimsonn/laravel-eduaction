1edf5fd1d0148a00c0d709c38ff27be6d9f8c4fc
ivan <luochenlong_1@hotmail.com>
2018-04-16 16:20:18 +1000

    ivan

---
+++ app/Http/Resources/StudentCourse.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Empire\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class StudentCourse extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
+}
